45133edb8182bbe3e8268acd703bea2ece121aae
Stijn Koopal <stijn.koopal@gmail.com>
2017-02-13 18:27:45 +0100

    Remove thymeleaf

--- src/main/java/de/codecentric/springbootsample/HomeController.java
+++ src/main/java/de/codecentric/springbootsample/HomeController.java
@@ -19,13 +19,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String home(ModelMap model) {
+    public @ResponseBody
+    String home(ModelMap model) {
         return "home";
     }
 }
